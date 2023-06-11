6ca31b31157390a9b38d5bea568c3b50e7ab9aee
SayBored <62549299+sayborduu@users.noreply.github.com>
2023-06-11 12:34:31 +0200

    Create claude.tsx

---
+++ app/components/claude.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClaudeAvatar = () => {
+  return (
+    <div className="user-avatar">
+      <img src="../icons/claude.svg" />
+    </div>
+  );
+};
+
+export default ClaudeAvatar;
