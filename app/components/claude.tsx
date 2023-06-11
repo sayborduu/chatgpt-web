3f99ab106d7e685c0af2f536c63a2b88e8fa0636
SayBored <62549299+sayborduu@users.noreply.github.com>
2023-06-11 12:38:25 +0200

    Update claude.tsx

--- app/components/claude.tsx
+++ app/components/claude.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ClaudeAvatar = () => {
   return (
     <div className="user-avatar">
-      <img src="../icons/claude.svg" />
+      <img src="https://psc2.cf2.poecdn.net/8dc3779437f03628e8a68e2ef0a55dd113315611/_next/static/media/anthropicAvatarBeige.426c3b88.png" class="user-avatar" />
     </div>
   );
 };
