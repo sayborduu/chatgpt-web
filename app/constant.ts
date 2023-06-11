55bf2e84fd86e89c3d683c84f6f96d96159fc223
SayBored <62549299+sayborduu@users.noreply.github.com>
2023-06-11 11:50:00 +0200

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
+export const OWNER = "sayborduu";
+export const REPO = "chatgpt-web";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
@@ -26,7 +26,7 @@ export enum FileName {
 }
 
 export enum StoreKey {
-  Chat = "chat-next-web-store",
+  Chat = "privgpt",
   Access = "access-control",
   Config = "app-config",
   Mask = "mask-store",
