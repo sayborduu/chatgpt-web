c4fd2fca9cfc1eb2484d0ae69106d625f5eb45ab
SayBored <62549299+sayborduu@users.noreply.github.com>
2023-06-13 08:45:42 +0200

    Create bard.tsx

---
+++ app/components/bard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const bard = () => {
+  return (
+    <div className="user-avatar">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Google_Bard_logo.svg" className="user-avatar" />
+    </div>
+  );
+};
+
+export default bard;
