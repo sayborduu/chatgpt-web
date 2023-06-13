fb651e9721272c3cc9deec742f7bc1853d227120
SayBored <62549299+sayborduu@users.noreply.github.com>
2023-06-13 08:57:53 +0200

    Actualizar bard.tsx

--- app/components/bard.tsx
+++ app/components/bard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BardAvatar = () => {
   return (
     <div className="user-avatar">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Google_Bard_logo.svg" className="user-avatar" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Google_Bard_logo.svg" width="30" height="30" className="user-avatar" />
     </div>
   );
 };
