0c0a3b3cdaa91e38b21e479aeecd17be3746140c
SayBored <62549299+sayborduu@users.noreply.github.com>
2023-06-13 09:20:24 +0200

    Actualizar bard.tsx

--- app/components/bard.tsx
+++ app/components/bard.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import "./bardstyle.css";
-import bardIcon from "../icons/bard.svg";
+import { ReactComponent as bardIcon } from "../icons/bard.svg";
 
 const BardAvatar = () => {
+    const svgString = new XMLSerializer().serializeToString(bardIcon);
   return (
     <div className="user-avatar">
       <img
-        src={bardIcon} 
+        src={`data:image/svg+xml;utf8,${encodeURIComponent(svgString)}`}
         className="uav-bard"
+        dangerouslySetInnerHTML={{ __html: svgString }}
       />
     </div>
   );
