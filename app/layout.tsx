a79067260338b0011f49e90e299646e31259b552
SayBored <62549299+sayborduu@users.noreply.github.com>
2023-06-18 13:02:08 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,28 +35,6 @@ export default function RootLayout({
         <meta name="version" content={buildConfig.commitId} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
-
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-J69GGWFT9G"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-J69GGWFT9G');
-        </script>
-        
-        <!-- Cookie Consent by TermsFeed https://www.TermsFeed.com -->
-        <script type="text/javascript" src="https://www.termsfeed.com/public/cookie-consent/4.0.0/cookie-consent.js" charset="UTF-8"></script>
-        <script type="text/javascript" charset="UTF-8">
-        document.addEventListener('DOMContentLoaded', function () {
-        cookieconsent.run({"notice_banner_type":"headline","consent_type":"implied","palette":"light","language":"en","page_load_consent_levels":["strictly-necessary","functionality","tracking","targeting"],"notice_banner_reject_button_hide":false,"preferences_center_close_button_hide":false,"page_refresh_confirmation_buttons":false,"website_name":"https://imalexbfs.ml","website_privacy_policy_url":"https://github.com/sayborduu/privgpt/blob/main/privacy%20policy.md"});
-        });
-        </script>
-        
-        <noscript>Free cookie consent management tool by <a href="https://www.termsfeed.com/" rel="nofollow noopener">TermsFeed Policy Generator</a></noscript>
-        <!-- End Cookie Consent by TermsFeed https://www.TermsFeed.com -->
-        
       </head>
       <body>{children}</body>
     </html>
