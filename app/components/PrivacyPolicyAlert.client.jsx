b85a44929cdf4cd453f7e5446a56253f111b59c5
SayBored <62549299+sayborduu@users.noreply.github.com>
2023-06-18 19:39:03 +0200

    Update PrivacyPolicyAlert.client.jsx

--- app/components/PrivacyPolicyAlert.client.jsx
+++ app/components/PrivacyPolicyAlert.client.jsx
@@ -11,7 +11,7 @@ const PrivacyPolicyAlert = () => {
     if (acceptedPrivacyPolicyVersion !== privacyPolicyVersion) {
       Swal.fire({
         title: 'Privacy Policy',
-        html: 'Our Privacy Policy has been updated. Please review and accept it from <a href="https://github.com/sayborduu/privgpt/blob/main/privacy%20policy.md">this link</a>.',
+        html: 'Our Privacy Policy has been updated. Please review and accept it from <a href="https://github.com/sayborduu/privgpt/blob/main/privacy%20policy.md" target="_blank" style="color:#000">this link</a>.',
         icon: 'info',
         confirmButtonText: 'Accept',
         allowOutsideClick: false, // Prevent closing the alert by clicking outside
